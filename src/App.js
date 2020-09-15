b8a8dd108d75de1ea4dd52b1fb1381d451e99f1e
waleedomega <waleedomega@hotmail.com>
2020-09-15 12:41:23 +0200

    about us

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react';
 
 import PageWrapper from './components/pageWrapper';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <PageWrapper>
-      <Router>
-        <Route
-          path='/home'
-          
-          />
-      </Router>
-        
-        
-      <Home/>
-    </PageWrapper>
-  );
+		<Router>
+			<PageWrapper>
+				<Route exact={true} path="/" component={Home} />
+				<Route path="/about" component={About} />
+			</PageWrapper>
+		</Router>
+	);
 }
 
 export default App;
