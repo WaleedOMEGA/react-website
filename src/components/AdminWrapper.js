ee1ba39945e4acb7a7bd8ea2bc0fef4393d9acf7
waleedomega <waleedomega@hotmail.com>
2020-09-27 13:33:45 +0200

    drawer

--- src/components/AdminWrapper.js
+++ src/components/AdminWrapper.js
@@ -10,43 +10,105 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
 const drawerwidth = 240;
-const styles = theme => ({
-    toolbar: {
-        paddingRight:24
-    }
-})
+const styles = (theme) => ({
+	toolbar: {
+		paddingRight: 24,
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		})
+	},
+	appBarShift: {
+		marginLeft: drawerwidth,
+		width: `calc(100% - ${drawerwidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		})
+	},
+	drawerPaper: {
+		position: 'relative',
+		whiteSpace: 'noWrap',
+		width: drawerwidth,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		})
+	},
+	drawerPaperClose: {
+		overflowX: 'hidden',
+		width: theme.spacing.unit * 7,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		})
+	},
+	toolbarIcon: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px',
+		...theme.mixins.toolbar,
+	},
+});
 class AdminWrapper extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            open:true
+        }
+    }
+    handleDrawerOpen = (e) => {
+        this.setState({ open: true });
+    }
+
+    handleDrawerClose = (e) => {
+        this.setState({open:false})
+    }
     render() {
         const { classes } = this.props;
         return (
-            <div id="admin-page">
-                <AppBar>
-                    <Toolbar className={classes.toolbar}>
-                        <IconButton>
-                        <MenuIcon />
-                        </IconButton>
-                        <Typography
-                            component='h1'
-                            variant='h6'
-                            color='inherit'
-                            noWrap
-                        >Admin</Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant='permanent'
-                    open={true}
-                >
-                    <list>
-                        <ListItem>
-                        Dashboard
-                        </ListItem>
-                    </list>
-                </Drawer>
-                {this.props.children}
-            </div>
-        )
+					<div id="admin-page">
+						<AppBar
+							className={classNames(
+								classes.appBar,
+								this.state.open &&
+								classes.appBarShift
+							)}
+						>
+							<Toolbar className={classes.toolbar}>
+								<IconButton onClick={this.handleDrawerOpen}>
+									<MenuIcon />
+								</IconButton>
+								<Typography component="h1" variant="h6" color="inherit" noWrap>
+									Admin
+								</Typography>
+							</Toolbar>
+						</AppBar>
+						<Drawer
+							classes={{ paper: classNames(classes.drawerPaper,!this.state.open && classes.drawerPaperClose) }}
+							variant="permanent"
+							open={true}
+						>
+							<div className={classes.toolbarIcon}>
+								<IconButton onClick={this.handleDrawerClose}>
+									<ChevronLeftIcon />
+								</IconButton>
+							</div>
+							<Divider />
+							<List>
+								<ListItem>Dashboard</ListItem>
+							</List>
+						</Drawer>
+						{this.props.children}
+					</div>
+				);
     }
 }
 
